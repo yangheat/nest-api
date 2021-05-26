2e7af237ae001f1fda03881c99b65711b1ad97c5
root <root@localhost.localdomain>
2021-05-27 01:02:25 +0900

    E2E TESTING

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,74 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Welcom to my Movie API');
+  });
+  
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+    
+    it('POST', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test Title',
+        year: 2021,
+        genres: ['test']
+      })
+      .expect(201);
+    });
+  
+    it('movies (DELETE)', () => {
+      return request(app.getHttpServer())
+      .delete('/movies')
+      .expect(404);
+    })
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+      .get('/movies/1')
+      .expect(200);
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+      .get('/movies/999')
+      .expect(404);
+    })
+
+    it.todo('DELETE');
+    it.todo('PATCH');
+  });
+});
