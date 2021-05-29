b38c3624ed3355a5142c0d37b842f7f737b33703
root <root@localhost.localdomain>
2021-05-29 17:50:22 +0900

    last commit

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -22,6 +22,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  // 웹사이트 방문
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
@@ -30,6 +31,7 @@ describe('AppController (e2e)', () => {
   });
   
   describe('/movies', () => {
+    // 전체 조회
     it('GET', () => {
       return request(app.getHttpServer())
         .get('/movies')
@@ -37,7 +39,8 @@ describe('AppController (e2e)', () => {
         .expect([]);
     });
     
-    it('POST', () => {
+    // 생성
+    it('POST 201', () => {
       return request(app.getHttpServer())
       .post('/movies')
       .send({
@@ -47,28 +50,56 @@ describe('AppController (e2e)', () => {
       })
       .expect(201);
     });
-  
-    it('movies (DELETE)', () => {
+
+    // 잘못된 어보로 생성
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test Title',
+        year: 2021,
+        genres: ['test'],
+        other: 'thing'
+      })
+      .expect(400);
+    });
+
+    // 모두 삭제
+    it('DELETE', () => {
       return request(app.getHttpServer())
       .delete('/movies')
       .expect(404);
-    })
+    });
   });
 
   describe('/movies/:id', () => {
+    // 한 개 조회
     it('GET 200', () => {
       return request(app.getHttpServer())
       .get('/movies/1')
       .expect(200);
     });
 
+    // 존재하지 않는 정보 조회
     it('GET 404', () => {
       return request(app.getHttpServer())
       .get('/movies/999')
       .expect(404);
     })
 
-    it.todo('DELETE');
-    it.todo('PATCH');
+    // 업데이트
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+      .get('/movies/1')
+      .send({ title: 'Update Test' })
+      .expect(200);
+    });
+
+    // 삭제
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+      .delete('/movies/1')
+      .expect(200);
+    });
   });
 });
